a28eef6479b8b7cb5431038948e58ad89eaef432
ArlanchikDrey <arlanchikdrey@yandex.ru>
2025-02-06 17:25:31 +0300

    Publish config changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("org.jetbrains.kotlin.jvm")
     id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.29.0"
 }
 
 version = properties["VERSION_NAME"].toString()
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
 
 kotlin {
     jvmToolchain(8)
@@ -20,41 +18,45 @@ dependencies {
 
 description = "Extensions pack for coroutines"
 
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            from(components["java"])
+publishing.publications
+    .withType<MavenPublication>()
+    .configureEach {
+        groupId = "io.github.arlanchikdrey"
 
-            groupId = "net.arlantech"
-            artifactId = "coroutineext"
+        pom {
+            name.set("CoroutineExt")
+            description.set(project.description)
+            url.set("https://github.com/ArlanchikDrey/CoroutineExt")
 
-            pom {
-                name.set("CoroutineExt")
-                description.set(project.description)
+            licenses {
+                license {
+                    name.set("The Apache License, Version 2.0")
+                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    distribution.set("repo")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("ArlanchikDrey")
+                    url.set("https://github.com/ArlanchikDrey")
+                }
+            }
+
+            issueManagement {
+                system.set("GitHub")
+                url.set("https://github.com/ArlanchikDrey/CoroutineExt/issues")
+            }
+
+            scm {
+                connection.set("scm:git:git://github.com/ArlanchikDrey/CoroutineExt.git")
+                developerConnection.set("scm:git:ssh://github.com/ArlanchikDrey/CoroutineExt.git")
                 url.set("https://github.com/ArlanchikDrey/CoroutineExt")
-
-                issueManagement {
-                    url.set("https://github.com/ArlanchikDrey/CoroutineExt/issues")
-                }
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        distribution.set("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("ArlanchikDrey")
-                        url.set("https://github.com/ArlanchikDrey")
-                    }
-                }
             }
         }
     }
 
+publishing {
     repositories {
         mavenLocal()
 
@@ -63,4 +65,9 @@ publishing {
             url = uri(rootProject.layout.buildDirectory.dir("maven-repo"))
         }
     }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
+    signAllPublications()
 }
